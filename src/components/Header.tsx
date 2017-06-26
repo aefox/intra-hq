7f4eaddf345cf451d17d257a79b192dffcc5101d
Nicolletta <grigorut_nicoleta@yahoo.com>
2017-06-26 17:32:50 +0300

    First commit on fork

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+import ".././styles/styles.scss";
+
+class Header extends React.Component<any, any> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="headerWithLogo">
+              <div className="logo">
+                <Logo logoPath="./src/images/logoCegeka.png"/>
+              </div>
+            </div>
+        );
+    }
+}
+
+const Logo = (props) => <img src={props.logoPath} alt="The Special Logo" />;
+
+export default Header;
