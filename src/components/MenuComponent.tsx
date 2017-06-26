82451e0984dc015248e355e2348f090a9fdbda9d
iustinan <iustina.necula@cegeka.com>
2017-06-26 18:23:48 +0300

    updated Menu components

--- src/components/MenuComponent.tsx
+++ src/components/MenuComponent.tsx
@@ -3,14 +3,18 @@ import * as ReactDom from "react-dom";
 import { MenuItemComponent } from "./MenuItemComponent";
 
 export const MenuComponent = () => {
-    const items = ["Menu"];
+    const items = ["Item1", "Item2", "Item3"];
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-xs-3">
                     <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
-                        <ul className="nav nav-pills flex-column">
-                            <MenuItemComponent values={items} />
+                        <ul className="nav flex-column">
+                            <li>
+                                <a className="navbar-brand">Menu</a></li>
+                            <li>
+                                <MenuItemComponent values={items} />
+                            </li>
                         </ul>
                     </nav>
                 </div>
--- src/components/MenuItemComponent.tsx
+++ src/components/MenuItemComponent.tsx
@@ -2,10 +2,10 @@ import * as React from "react";
 import * as ReactDom from "react-dom";
 
 export const MenuItemComponent = (props, { }) => {
-    const items = props.values.map(function (value) {
-        return (<li className="nav-item">
+    const items = props.values.map( (value, index) => {
+        return (<li key={index} className="nav-item">
             <a className="nav-link" href="#">{value}</a>
-        </li>)
+        </li>);
     });
-    return (<ul>{items}</ul>);
+    return (<ul className="nav nav-pills flex-column">{items}</ul>);
 };
