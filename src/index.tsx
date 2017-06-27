78044022348928c10cbb83e6cfe713a22eec3295
Vitel Bogdan <bogdan.vitel@cegeka.com>
2017-06-27 10:48:12 +0300

    Initial implementation of search bar component

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Hello } from "./components/Hello";
+import SearchBar from "./components/SearchBar";
 
 import "bootstrap/scss/bootstrap.scss";
 
@@ -9,6 +10,7 @@ const App = () => {
     <div className="jumbotron jumbotron-fluid">
         <h1 className="display-3">Fluid jumbotron from bootstrap!</h1>
         <Hello compiler="TypeScript" framework="React" />
+        <SearchBar/>
     </div>
   );
 };
--- stories/hello.js
+++ stories/hello.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import SearchBar from "../src/components/SearchBar";
 // import { action } from '@storybook/addon-actions';
 
 import { Hello } from "../src/components/Hello";
@@ -7,4 +8,9 @@ import { Hello } from "../src/components/Hello";
 storiesOf("Hello", module)
   .add("with some text", () => (
     <Hello compiler="TypeScript" framework="React" />
+  ));
+
+storiesOf("search bar", module)
+  .add("regular", () => (
+    <SearchBar />
   ));
