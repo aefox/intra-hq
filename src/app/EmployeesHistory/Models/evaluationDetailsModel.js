1f1c7bd334770a21529519fc83cd0a0d2ad4952c
CEGEKANV\alexandb <alexandru.banaru@cegeka.com>
2017-07-31 18:00:45 +0300

    Employee history section Iustina and AlexB

---
+++ src/app/EmployeesHistory/Models/evaluationDetailsModel.js
@@ -0,0 +1,8 @@
+class EvaluationDetailsModel {
+  constructor(evaluationDate, feedback) {
+    this.evaluationDate = evaluationDate;
+    this.feedback = feedback;
+  }
+}
+
+export default EvaluationDetailsModel;
---
+++ src/app/EmployeesHistory/Models/projectTimelineModel.js
@@ -0,0 +1,9 @@
+class ProjectTimelineModel {
+  constructor(name, startDate, endDate) {
+    this.name = name;
+    this.startDate = startDate;
+    this.endDate = endDate;
+  }
+}
+
+export default ProjectTimelineModel;
