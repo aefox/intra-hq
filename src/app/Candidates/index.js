54c2775b75318f400cc14da3e39c1d506239eaeb
CEGEKANV\anamarib <Ana-Maria.Boar@cegeka.com>
2017-07-31 17:28:31 +0300

    Add components to display the list of candidates and their details

--- src/app/Candidates/index.js
+++ src/app/Candidates/index.js
@@ -1,10 +1,76 @@
 import React from 'react';
+import './styles.css';
 
-const Mock = ({ match }) =>
-  <div>
-    <p>
-      {match.url}
-    </p>
+const Candidate = ({ candidate, setCandidateId }) =>
+  <div
+    className="candidate-name"
+    onClick={() => {
+      setCandidateId(candidate.id);
+    }}
+  >
+    {candidate.name}
   </div>;
 
-export default Mock;
+const Details = ({ candidate }) =>
+  <div>
+    <div>
+      {candidate.name}
+    </div>
+    <div>
+      {candidate.phoneNumber}
+    </div>
+    <div>
+      {candidate.yearsExperience}
+    </div>
+  </div>;
+
+class Candidates extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      candidates: [],
+      candidateId: null
+    };
+
+    this.setCandidateId = this.setCandidateId.bind(this);
+  }
+
+  setCandidateId(candidateId) {
+    this.setState({ candidateId });
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:9000/candidates')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ candidates: data });
+        return data;
+      })
+      .catch(errorData => console.error(errorData));
+  }
+
+  render() {
+    const id = this.state.candidateId;
+    return this.state.candidates.length > 0
+      ? <div className="candidates">
+          <div className="candidates-list">
+            {this.state.candidates.map(candidate =>
+              <Candidate
+                setCandidateId={this.setCandidateId}
+                key={candidate.id}
+                candidate={candidate}
+              />
+            )}
+          </div>
+          <div className="candidate-details">
+            {id
+              ? <Details candidate={this.state.candidates[id - 1]} />
+              : 'No Candidate selected'}
+          </div>
+        </div>
+      : <span>NO CANDIDATES</span>;
+  }
+}
+
+export default Candidates;
